baf9bdc511f0943aa77351119f2f5af366d82593
Jeongeon <wjddjs0729@gmail.com>
2022-03-14 16:13:33 +0900

    feat Intro

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,5 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+
+import Intro from "../views/Intro.vue";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
@@ -7,21 +9,18 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: Home,
+    name: "Intro",
+    component: Intro,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/main",
+    name: "Home",
+    component: Home,
   },
 ];
 
 const router = new VueRouter({
+  mode: "history",
   routes,
 });
 
---
+++ Frontend/src/views/Intro.vue
@@ -0,0 +1,87 @@
+<template>
+  <v-app id="intro">
+    <v-img
+      :min-height="'calc(100vh - ' + $vuetify.application.top + 'px)'"
+      src="https://cdn.imweb.me/upload/S2017101359e025984d346/671b7600c2bf9.jpg"
+    >
+      <v-theme-provider dark>
+        <v-container fill-height>
+          <v-row align="center" class="white--text mx-auto" justify="center">
+            <v-col class="white--text text-center" cols="12" tag="h1">
+              <span
+                :class="[
+                  $vuetify.breakpoint.smAndDown ? 'display-2' : 'display-3',
+                ]"
+                class="font-weight-black"
+              >
+                잠깐만요!
+              </span>
+
+              <br />
+              <br />
+
+              <span
+                :class="[
+                  $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                ]"
+                class="font-weight-light"
+              >
+                만 19세 이상인가요?
+              </span>
+
+              <br />
+              <br />
+              <v-btn text>
+                <span
+                  :class="[
+                    $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                  ]"
+                  class="font-weight-black"
+                  @click="gomain"
+                >
+                  Yes
+                </span>
+              </v-btn>
+
+              <span
+                :class="[
+                  $vuetify.breakpoint.smAndDown ? 'display-0' : 'display-1',
+                ]"
+                class="font-weight-light"
+              >
+                or
+              </span>
+
+              <v-btn text>
+                <span
+                  :class="[
+                    $vuetify.breakpoint.smAndDown ? 'display-1' : 'display-2',
+                  ]"
+                  class="font-weight-black"
+                >
+                  No
+                </span>
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-theme-provider></v-img
+    >
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Intro",
+  components: {},
+
+  methods: {
+    gomain() {
+      console.log("click");
+      this.$router.push("/main");
+    },
+  },
+};
+</script>
+
+<style></style>
