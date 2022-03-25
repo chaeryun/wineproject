c198247c33746ee267b4a869c06667963e0df785
kimseonghyun <gkska741@kaist.ac.kr>
2022-03-25 10:46:35 +0900

    FEAT : signup 수정

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -2,10 +2,9 @@ from django.http import JsonResponse
 
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
-from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from .models import User
 from .serializers import UserSerializer
 
@@ -37,9 +36,9 @@ class AccountManagement(APIView):
 
     #회원정보 저장(회원가입)
     @api_view(["POST"])
+    @permission_classes([AllowAny])
     def signup(request):
         serializer = UserSerializer(data=request.data)
-
         if serializer.is_valid(raise_exception=True):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
