e46332b076e063424d659c366165279170ada0ce
kimseonghyun <gkska741@kaist.ac.kr>
2022-04-07 14:43:05 +0900

    BE:username

--- backend/wine/urls.py
+++ backend/wine/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('get_wine_list/', views.get_wine_list),
     path('update_wine_data/', views.update_wine_data),
     path('add_wine_wishlist/<int:wine_id>/<str:username>/', views.add_wine_wishlist),
-    path('latest_wine_list/<int:user_id>/', views.latest_wine_list), #최근에 내가 찜한 와인 리스트
-    path('latest_wine_totallist/<int:user_id>/', views.latest_wine_totallist),
+    path('latest_wine_list/<str:username>/', views.latest_wine_list), #최근에 내가 찜한 와인 리스트
+    path('latest_wine_totallist/<str:username>/', views.latest_wine_totallist),
 
     path('recommand/latest_wishlist/<int:user_id>/', views.reco_wishlist), #찜 기반 와인 추천 
     path('recommand/similar_wine/<int:wine_id>/', views.reco_similar_wine), #단일 와인-> 유사와인 추천
